f0a1786cba1095f37ebb180840f95705bf2ce44f
garrethil <garrethildebrandt@gmail.com>
2024-11-07 14:56:47 -0500

    add Login route for existing users at /auth/login with JWT auth

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import { User } from "./models/User";
 
 dotenv.config();
@@ -66,9 +67,80 @@ app.post("/auth/register", async (req, res) => {
     });
   }
 });
-// Test route
-app.get("/", (req: Request, res: Response) => {
-  res.send("<h1>Welcome To JWT Authentication</h1>");
+
+app.post("/auth/login", async (req, res) => {
+  try {
+    // ** Get The User Data From Body ;
+    const user = req.body;
+
+    // ** destructure the information from user;
+    const { email, password } = user;
+
+    // ** Check the (email/user) exist  in database or not ;
+    const isUserExist = await User.findOne({
+      email: email,
+    });
+
+    // ** if there is not any user we will send user not found;
+    if (!isUserExist) {
+      res.status(404).json({
+        status: 404,
+        success: false,
+        message: "User not found",
+      });
+      return;
+    }
+
+    // ** if the (user) exist  in database we will check the password is valid or not ;
+    // **  compare the password in db and the password sended in the request body
+
+    const isPasswordMatched = isUserExist?.password === password;
+
+    // ** if not matched send response that wrong password;
+
+    if (!isPasswordMatched) {
+      res.status(400).json({
+        status: 400,
+        success: false,
+        message: "wrong password",
+      });
+      return;
+    }
+
+    // ** if the email and password is valid create a token
+
+    /*
+    To create a token JsonWebToken (JWT) receive's 3 parameter
+    1. Payload -  This contains the claims or data you want to include in the token.
+    2. Secret Key - A secure key known only to the server used for signing the token.
+    3. expiration -  Additional settings like token expiration or algorithm selection.
+    */
+
+    // !! Don't Provide the secret openly, keep it in the .env file. I am Keeping Open just for demonstration
+
+    // ** This is our JWT Token
+    const token = jwt.sign(
+      { _id: isUserExist?._id, email: isUserExist?.email },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // send the response
+    res.status(200).json({
+      status: 200,
+      success: true,
+      message: "login success",
+      token: token,
+    });
+  } catch (error: any) {
+    // Send the error message to the client
+    res.status(400).json({
+      status: 400,
+      message: error.message.toString(),
+    });
+  }
 });
 
 // Connect to MongoDB and Start Server
