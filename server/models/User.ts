f7d35fc80dbe4ad947e99481bad856590eb99bb6
garrethil <garrethildebrandt@gmail.com>
2024-11-07 14:03:58 -0500

    Create secure user registration endpoint at /auth/register for new users in mongoDB

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,41 +1,12 @@
-import mongoose, { Schema, Document, Types } from "mongoose";
-import bcrypt from "bcrypt";
+// models/user.ts
+import mongoose from "mongoose";
 
-// Define TypeScript interface
-interface IUser extends Document {
-  _id: Types.ObjectId;
-  email: string;
-  passwordHash: string;
-  creations: Types.ObjectId[];
-  comparePassword(password: string): Promise<boolean>;
-}
-
-// Use interface in Schema
-const userSchema = new Schema<IUser>({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
   },
-  passwordHash: {
-    type: String,
-    required: true,
-  },
-  creations: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Creation", // to be updated when creation model is made
-    },
-  ],
-});
+  { timestamps: true }
+);
 
-// Add the comparePassword method to the schema
-userSchema.methods.comparePassword = async function (
-  password: string
-): Promise<boolean> {
-  return await bcrypt.compare(password, this.passwordHash);
-};
-
-const User = mongoose.model<IUser>("User", userSchema);
-
-export default User;
+export const User = mongoose.model("User", UserSchema);
