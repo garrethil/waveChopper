2a909d38b7a145dd56b857fd31639265f8bffeef
garrethil <garrethildebrandt@gmail.com>
2025-01-29 12:34:22 -0500

    whitlist all ips for db access

--- server/middleware/index.ts
+++ server/middleware/index.ts
@@ -6,7 +6,7 @@ export const registerMiddleware = (app: Application) => {
     cors({
       origin: process.env.FRONTEND_URL || "http://localhost:5173",
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true, // Allow cookies & auth headers
+      credentials: true,
       allowedHeaders: ["Content-Type", "Authorization"],
     })
   );
