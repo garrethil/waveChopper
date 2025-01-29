bc12dcb76db72bd6c1c130020537901ae34c4d57
garrethil <garrethildebrandt@gmail.com>
2025-01-29 12:25:15 -0500

    fix cors issue

--- server/middleware/index.ts
+++ server/middleware/index.ts
@@ -2,7 +2,14 @@ import cors from "cors";
 import express, { Application } from "express";
 
 export const registerMiddleware = (app: Application) => {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL || "http://localhost:5173",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true, // Allow cookies & auth headers
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 };
