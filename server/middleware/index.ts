fee49f848de4c2e71c8e6b6f2c05f34268c0261f
garrethil <garrethildebrandt@gmail.com>
2025-01-29 12:56:33 -0500

    add both urls to cors allow

--- server/middleware/index.ts
+++ server/middleware/index.ts
@@ -4,6 +4,7 @@ import express, { Application } from "express";
 // Allow requests only from your deployed frontend
 const allowedOrigins = [
   "https://wave-chopper-2dc5d4458dd7.herokuapp.com", // Frontend URL
+  "https://wave-chopper.herokuapp.com", // Backend URL
 ];
 
 export const registerMiddleware = (app: Application) => {
